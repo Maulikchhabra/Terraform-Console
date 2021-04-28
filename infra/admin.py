1f16626896c50fcbad03adae528a3b0f62bf3374
Shashwat Kumar <shashwat9kumar@gmail.com>
2021-04-28 15:58:28 +0530

    Add files via upload

--- infra/admin.py
+++ infra/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import KeyModel, InstanceModel
+from .models import KeyModel, InstanceModel, VPCModel, SubnetModel, NetworkInterfaceModel
 
 
 # Register your models here.
 
 admin.site.register(KeyModel)
 admin.site.register(InstanceModel)
+admin.site.register(VPCModel)
+admin.site.register(SubnetModel)
+admin.site.register(NetworkInterfaceModel)
--- infra/forms.py
+++ infra/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from .models import InstanceModel
+from .models import InstanceModel, VPCModel, SubnetModel, NetworkInterfaceModel
 
 
 class KeyForm(forms.Form):
@@ -10,28 +10,28 @@ class KeyForm(forms.Form):
     region = forms.CharField(label='Region', max_length=100)
 
 
-class InstanceForm(forms.Form):
-    name = forms.CharField(label='Instance Name', max_length=100)
-    ami = forms.CharField(label='Instance AMI', max_length=100)
-    instancetype = forms.CharField(label='Instance Type', max_length=100)
+# class InstanceForm(forms.Form):
+#     name = forms.CharField(label='Instance Name', max_length=100)
+#     ami = forms.CharField(label='Instance AMI', max_length=100)
+#     instancetype = forms.CharField(label='Instance Type', max_length=100)
 
 
 class InstanceModelForm(ModelForm):
     class Meta:
         model = InstanceModel
-        fields = ['name', 'ami', 'instancetype']
+        fields = ['name', 'ami', 'instancetype', 'netint']
 
         AmiChoices = (
-            ('AXYZA', 'Ubuntu 1'),
+            ('ami-0885b1f6bd170450c', 'Ubuntu "us-east-1"'),
             ('BYXTA', 'Ubuntu 2'),
             ('CXYZA', 'Ubuntu 3'),
         )
 
         InstanceTypeChoices = (
-            ('t2-micro', 't2-micro'),
-            ('t2-macro', 't2-macro'),
-            ('t3-micro', 't3-micro'),
-            ('t3-macro', 't3-macro'),
+            ('t2.micro', 't2.micro'),
+            ('t2.macro', 't2.macro'),
+            ('t3.micro', 't3.micro'),
+            ('t3.macro', 't3.macro'),
         )
 
         widgets = {
@@ -44,3 +44,21 @@ class InstanceModelForm(ModelForm):
             #     'class': 'form-control'
             # }),
         }
+
+
+class VPCModelForm(ModelForm):
+    class Meta:
+        model = VPCModel
+        fields = ['name', 'cidrblock']
+
+
+class SubnetModelForm(ModelForm):
+    class Meta:
+        model = SubnetModel
+        fields = ['name', 'vpcid', 'cidrblock']
+
+
+class NetworkInterfaceModelForm(ModelForm):
+    class Meta:
+        model = NetworkInterfaceModel
+        fields = ['name', 'subnetid', 'privateips']
